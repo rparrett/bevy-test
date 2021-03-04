f17b9320d7b467a543ff6992fbdecdcd49141285
Rob Parrett <robparrett@gmail.com>
2021-03-04 11:01:12 -0700

    bevy-1552

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
-use bevy::{prelude::*};
+use bevy::prelude::*;
 
 fn main() {
     App::build()
         .add_plugins(DefaultPlugins)
+        .add_startup_system(setup.system())
         .run();
+}
+
+fn setup(
+    commands: &mut Commands,
+    asset_server: Res<AssetServer>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    let texture_handle = asset_server.load("branding/icon.png");
+    commands
+        .spawn(OrthographicCameraBundle::new_2d())
+        .spawn(SpriteBundle {
+            material: materials.add(texture_handle.into()),
+            ..Default::default()
+        });
 }
