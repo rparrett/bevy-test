6b42144a16fa329bd3ccff0c6f247172a7dd026c
Rob Parrett <robparrett@gmail.com>
2021-04-12 10:33:42 -0700

    Simple sprite scaling culling failure case

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,37 @@
-use bevy::{prelude::*};
+use bevy::{prelude::*, sprite::SpriteSettings};
+
+struct Bar;
+
+fn setup(
+    mut commands: Commands,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+
+    let mut transform = Transform::from_xyz(-800.0, 0.0, 0.0);
+    transform.scale = Vec3::new(1.0, 20.0, 1.0);
+
+    commands.spawn_bundle(SpriteBundle {
+        material: materials.add(Color::rgb(0.5, 0.5, 1.0).into()),
+        transform,
+        sprite: Sprite::new(Vec2::new(30.0, 30.0)),
+        ..Default::default()
+    }).insert(Bar);
+}
+
+fn rotate(mut query: Query<&mut Transform, With<Bar>>, time: Res<Time>) {
+    for mut t in query.iter_mut() {
+        t.rotation *= Quat::from_rotation_z(time.delta_seconds());
+    }
+}
 
 fn main() {
     App::build()
+        .insert_resource(SpriteSettings {
+            frustum_culling_enabled: true,
+        })        
         .add_plugins(DefaultPlugins)
+        .add_startup_system(setup.system())
+        .add_system(rotate.system())
         .run();
 }
